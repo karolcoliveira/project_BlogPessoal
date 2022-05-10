9288055f9b0ac1455da2bc407bb89c3f705db8a8
karolcoliveira <oliveiraakarol@hotmail.com>
2022-05-10 19:16:18 -0300

    @karolcoliveira [Task: Create utilities and add Enum] {10/05/22 19:16}

---
+++ BlogPessoal/src/utilities/Enum.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlogPessoal.src.utilities
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TypeUser
+    {
+        NORMAL,
+        ADMINISTRATOR
+    }
+}
